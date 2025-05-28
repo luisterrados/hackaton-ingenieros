d0a86b3cab33895e8930199cfaeff58bea606b6d
zhuzhangjunhao <junhao.zhu@alumnos.upm.es>
2025-05-28 10:28:06 +0200

    Fronted basico creado

---
+++ src/lib/components/ContainerCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    let {
+        container
+    } = $props();
+
+
+</script>
+
+<div class="p-4 border rounded mb-2">
+    <p><strong>Tipo:</strong> {container.tipo}</p>
+    <p><strong>Centro:</strong> {container.centro}</p>
+    <p><strong>Localización:</strong> {container.localizacion}</p>
+    <p><strong>Capacidad:</strong> {container.capacidad} litros</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+    import {onMount} from "svelte";
+    import ContainerCard from "$lib/components/ContainerCard.svelte";
+    let contenedores: Array<any> = [];
+
+    // Fetch al cargar el componente
+    onMount(async () => {
+        const res = await fetch('/data/containers.json');
+        contenedores = await res.json();
+    });
+</script>
+
+<style>
+    .contenedor-principal {
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 1rem;
+    }
+
+    h1 {
+        text-align: center;
+        font-size: 2.5rem;
+        margin-bottom: 2rem;
+    }
+
+    .contenedor {
+        padding: 1rem;
+        border: 1px solid #ccc;
+        border-radius: 0.5rem;
+        margin-bottom: 1rem;
+    }
+
+
+</style>
+
+<h1>Contenedores</h1>
+
+{#each contenedores as contenedor}
+    <ContainerCard container={contenedor}>
+
+    </ContainerCard>
+{/each}
